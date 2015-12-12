e8e1002ab2508eb75edbd3e29da9862f55a51492
Ben Zimmer <benjamin.zimmer@infopark.de>
2015-12-12 22:02:30 +0100

    update test configurations

--- spec/feature_spec_helper.rb
+++ spec/feature_spec_helper.rb
@@ -4,8 +4,8 @@ require 'rack/test'
 require File.expand_path('../../app', __FILE__)
 
 module RackSpecHelpers
-    include Rack::Test::Methods
-    attr_accessor :app
+  include Rack::Test::Methods
+  attr_accessor :app
 end
 
 RSpec.configure do |c|
